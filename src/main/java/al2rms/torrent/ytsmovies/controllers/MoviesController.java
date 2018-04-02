056c8a0d8de23a94c10e087b0d8cbe9234725853
Leonardo2rms <jleodosramos@gmail.com>
2018-04-02 19:39:00 -0300

    Llamada al servicio que obtiene todas las peliculas

---
+++ src/main/java/al2rms/torrent/ytsmovies/controllers/MoviesController.java
@@ -0,0 +1,35 @@
+package al2rms.torrent.ytsmovies.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@RestController()
+@RequestMapping("/movies")
+public class MoviesController {
+
+    private static final Logger log = LoggerFactory.getLogger(MoviesController.class);
+
+    @GetMapping()
+    public String getMovies(){
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+        Object response = restTemplate.exchange("https://yts.am/api/v2/list_movies.json", HttpMethod.GET,entity,Object.class);
+        log.info(response.toString());
+        return response.toString();
+    }
+}
---
+++ src/main/java/al2rms/torrent/ytsmovies/pojo/YtsResponse.java
@@ -0,0 +1,31 @@
+package al2rms.torrent.ytsmovies.pojo;
+
+public class YtsResponse {
+
+    private String status;
+    private String status_message;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus_message() {
+        return status_message;
+    }
+
+    public void setStatus_message(String status_message) {
+        this.status_message = status_message;
+    }
+
+    @Override
+    public String toString() {
+        return "YtsResponse{" +
+                "status='" + status + '\'' +
+                ", status_message='" + status_message + '\'' +
+                '}';
+    }
+}
