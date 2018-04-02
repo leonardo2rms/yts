660890089ef238d2f3c2826ec6e84416e9d2f170
Leonardo2rms <jleodosramos@gmail.com>
2018-04-02 18:03:41 -0300

    Initial commit: base application with DBC and Swagger API

---
+++ src/main/java/al2rms/torrent/ytsmovies/YtsMoviesApplication.java
@@ -0,0 +1,12 @@
+package al2rms.torrent.ytsmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtsMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtsMoviesApplication.class, args);
+	}
+}
