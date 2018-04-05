f86943ada9206fddc034ed1bcd2c64c490088554
Leonardo Dos Ramos <jleodosramos@gmail.com>
2018-04-04 22:37:17 -0300

    Agregar archivo utils

--- src/main/java/al2rms/torrent/ytsmovies/controllers/MoviesController.java
+++ src/main/java/al2rms/torrent/ytsmovies/controllers/MoviesController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -21,6 +22,20 @@ public class MoviesController {
 
     @GetMapping()
     public String getMovies(){
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+        Object response = restTemplate.exchange("https://yts.am/api/v2/list_movies.json", HttpMethod.GET,entity,Object.class);
+        log.info(response.toString());
+        return response.toString();
+    }
+
+    @GetMapping("/{movieId}")
+    public String getMovie(@PathVariable String movieId){
 
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
---
+++ src/main/java/al2rms/torrent/ytsmovies/general/utils/Utils.java
@@ -0,0 +1,8 @@
+package al2rms.torrent.ytsmovies.general.utils;
+
+
+public class Utils {
+    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36";
+
+
+}
