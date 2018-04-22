82374f59f07ccb2f48dc121df6c1c3774dff0d38
Leonardo2rms <jleodosramos@gmail.com>
2018-04-22 15:29:51 -0300

    cambiar anotacion

--- src/main/java/al2rms/torrent/ytsmovies/repositories/MovieRepositorie.java
+++ src/main/java/al2rms/torrent/ytsmovies/repositories/MovieRepositorie.java
@@ -2,8 +2,8 @@ package al2rms.torrent.ytsmovies.repositories;
 
 import al2rms.torrent.ytsmovies.pojo.Movie;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface MovieRepositorie extends CrudRepository<Movie, String> {
 }
